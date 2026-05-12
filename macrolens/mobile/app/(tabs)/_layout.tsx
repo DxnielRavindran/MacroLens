5dafbd2b6f3bed9f1246c3d6ab8b3b11d6b29897
Daniel Ravindran <ravin062@umn.edu>
2026-05-12 16:37:53 -0500

    add tab navigator along with dashboard, camera, and history tabs

--- macrolens/mobile/app/(tabs)/_layout.tsx
+++ macrolens/mobile/app/(tabs)/_layout.tsx
@@ -17,16 +17,23 @@ export default function TabLayout() {
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
-        name="index"
+        name="dashboard"
         options={{
-          title: 'Home',
+          title: 'Dashboard',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="camera"
         options={{
-          title: 'Explore',
+          title: 'Camera',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: 'History',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
